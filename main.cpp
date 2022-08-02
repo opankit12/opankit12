762ab3210fb120c491451fb452258dfe030d8461
opankit12 <106423119+opankit12@users.noreply.github.com>
2022-08-02 20:42:51 +0530

    positive or not

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a;
+   cout<<"enter the value of a"<<endl;
+   cin>>a;
+   
+   if(a>0){
+       cout<<"positive value"<<endl;
+   }
+   else if(a<0){
+       cout<<"negative value"<<endl;
+   }
+   else{
+       cout<<"no is zero"<<endl;
+   }       
+   }
