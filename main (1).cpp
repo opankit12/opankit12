41a40c007071a203c07cd6038307ad3b8f1f3f8e
opankit12 <106423119+opankit12@users.noreply.github.com>
2022-08-02 22:06:34 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=2;
+    int sum=0;
+    while(i<=n){
+        sum=sum+i;
+        i=i+2;
+        
+    }
+    
+    cout<<sum<<endl;
+    
+}
