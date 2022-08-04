5b30628127d0658e67e4b5bd945931f87ce15d27
opankit12 <106423119+opankit12@users.noreply.github.com>
2022-08-04 17:52:30 +0530

    pattern 3

---
+++ main (4).cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+ # include<iostream>
+    using namespace std;
+    
+    int main( )
+    {
+        int n;
+        cin>>n;
+        int row=1;
+        
+        while(row<=n){
+            int col = 1;
+            while(col<=row){
+                cout<< "$";
+                
+                col=col+1;
+            }
+            cout<<endl;
+            row=row+1;
+        }
+        
+    }
