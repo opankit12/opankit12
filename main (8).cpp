2aa041daafa1be0fd90dace28c26d97254eff891
opankit12 <106423119+opankit12@users.noreply.github.com>
2022-08-06 17:54:30 +0530

    pattern 7

---
+++ main (8).cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+    #include<iostream>
+    using namespace std;
+    
+    int main()
+    {
+        int n;
+        cin>>n;
+        
+        int row = 1;
+        while(row<=n){
+            
+            int col;
+            col=1;
+            while(col<=row){
+                
+                cout<<row-col+1;
+                col=col+1;
+            }
+            cout<<endl;
+            row=row+1;
+        }
+        
+        
+        
+        
+    }
